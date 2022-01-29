eaa9d838c6ad6c74fdac941b11a4362fbf502d5d
Mohamed <mohamed.bouhleel@gmail.com>
2022-01-29 18:32:56 +0100

    install vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {reactive, ref} from "vue";
+
+const apis = reactive([])
+const lastUrl = ref(null);
+</script>
+
+<template>
+ <input type="url" v-model="lastUrl" />
+  <button @click="apis.push(lastUrl)">Ajouter une api a surveiller</button>
+  <div v-for="api in apis" :key="api">
+    {{ api }}
+  </div>
+</template>
+
+
