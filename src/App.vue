74254c077ec1e19542ef10d722e62b62cc1ef2cc
Mohamed <mohamed.bouhleel@gmail.com>
2022-01-29 18:56:01 +0100

    api monitoring

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <script setup>
-import {reactive, ref} from "vue";
+import {onMounted, onUpdated, reactive, ref} from "vue";
+import axios from "axios";
 
 const apis = reactive([])
 const lastUrl = ref(null);
+
+onUpdated(() => {
+  if(apis.length > 0){
+    setInterval(function(){
+      apis.map(api =>
+      {
+        console.log('hoho')
+        axios.get(api?.url).then(res => {
+          api.status = res.status
+          if(api.status === 200){
+            api.color = 'green'
+          }else {
+            api.color = 'red'
+          }
+        })
+        .catch(res => {
+          api.color = 'red'
+        })
+        return api
+      });
+
+    }, 2000)
+  }
+})
+
+
 </script>
 
 <template>
  <input type="url" v-model="lastUrl" />
-  <button @click="apis.push(lastUrl)">Ajouter une api a surveiller</button>
-  <div v-for="api in apis" :key="api">
-    {{ api }}
+  <button @click="apis.push({url: lastUrl})">Ajouter une api a surveiller</button>
+  <div v-for="api in apis" :key="api" :style="'background:' + api.color ">
+    {{ api.url }}
+    {{ api.status }}
   </div>
 </template>
 
